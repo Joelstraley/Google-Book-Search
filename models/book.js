3e00cc4f22e9aeeccb3d6a66d697ce1af94aaa6a
Joel Straley <joelstraley@gmail.com>
2021-04-12 17:24:42 -0400

    bulding navbar and front page

---
+++ models/book.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+  },
+
+  authors: {
+    type: String,
+    trim: true,
+  },
+
+  description: {
+    type: String,
+  },
+
+  image: {
+    type: String,
+  },
+
+  link: {
+    type: String,
+  }
+
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes")
+const mongoose = require("mongoose");
+const { allowedNodeEnvironmentFlags } = require("process");
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +14,17 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", 
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
 
 // Define API routes here
 
